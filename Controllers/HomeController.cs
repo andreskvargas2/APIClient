23a5fe57cd9f54dc140c36ea53b287ca58a76c31
Andres Camilo Vargas Ascencio <andresk22_@hotmail.com>
2021-09-08 09:43:33 -0500

    Se agrega el proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using PruebaTecnicaSxo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using static APICliente.ViewModels.Result;
+
+namespace APICliente.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var urlAPI = WebConfigurationManager.AppSettings["urlAPI"];
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync(urlAPI+"getFacturas");
+            List<Factura> listFacturas = JsonConvert.DeserializeObject<List<Factura>>(json);
+
+            return View(listFacturas);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AgregarFactura(Factura nuevaFactura)
+        {
+            var urlAPI = WebConfigurationManager.AppSettings["urlAPI"];
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(nuevaFactura);
+            StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(urlAPI+ "postNuevaFactura", httpContent);
+            if (response.IsSuccessStatusCode)
+                return Json(new ResultHttpRequest { estado = true }, JsonRequestBehavior.AllowGet);
+            else
+                return Json(new ResultHttpRequest { estado = false}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ViewModels/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APICliente.ViewModels
+{
+    public class Result
+    {
+        public class ResultHttpRequest
+        {
+            public bool estado { get; set; }
+            public int codigo { get; set; }
+            public string descripción { get; set; }
+            
+        }
+    }
+}
